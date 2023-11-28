f5feb0723ad448e65de908496b28ba90ef317206
İbrahim EDİZ <edizdesign@gmail.com>
2023-11-28 13:03:09 +0000

    28112023_7

--- ilk.py
+++ ilk.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 liste = ["HakanOzden","HakanOzel","GamzeCelik","FurkanKeles","MerveSarpBoyar","OzgeCillik","SimgeAydin","IpekDolu"]
-fileName = "02_02_Donguler.ipynb"
+fileName = "02_03_Donguler.ipynb"
 for item in liste:
     if not os.path.exists(f"Egzersizler/{item}"):
         os.mkdir(f"Egzersizler/{item}")
