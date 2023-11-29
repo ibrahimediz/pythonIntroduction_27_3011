f81f433149804f6244ed71c02fa2e06a95a78582
İbrahim EDİZ <edizdesign@gmail.com>
2023-11-29 11:58:52 +0000

    29112023_5

--- deneme.py
+++ deneme.py
@@ -31,3 +31,67 @@ print(*sys.path,sep="\n")
 #     """
 #     print("Merhaba")
 # fonk
+
+# def topla(a,b):
+#     islem = "+"
+#     return a,b,a+b,islem
+# def cikar(a,b):
+#     islem = "-"
+#     return a,b,a-b,islem
+# def bolme(a,b):
+#     islem = "/"
+#     return a,b,a/b,islem
+# def carp(a,b):
+#     islem = "*"
+#     return a,b,a*b,islem
+# def kuvvet(a,b):
+#     islem = "**"
+#     return a,b,a**b,islem
+# def modulus(a,b):
+#     islem = "%"
+#     return a,b,a%b,islem
+
+
+
+def hesapMak():
+    menu = """
+    ##########
+    1. Toplama
+    2. Çıkarma
+    3. Bölme
+    4. Çarpma
+    5. Kuvvet
+    6. Mode Alma
+    7. Çıkış
+    İşlem Seçiniz:
+    """
+    # (lambda x,y:x,y,x+y,"+")
+    # fonkList = [topla,cikar,bolme,carp,kuvvet,modulus]
+    fonkList=[(lambda x,y:(x,y,x+y,"+")), # toplama
+    (lambda x,y:(x,y,x-y,"-")), # çıkarma
+    (lambda x,y:(x,y,x/y,"/")), # bölme
+    (lambda x,y:(x,y,x*y,"*")), # carpma
+    (lambda x,y:(x,y,x**y,"**")), # kuvvet
+    (lambda x,y:(x,y,x%y,"%"))] # modulus
+    #            0.     1.    2.    3.       4.     5. 
+    islem = 0
+    while islem != 7:
+        islem = input(menu)
+        if islem and islem.isdigit():
+            islem = int(islem)
+            if islem in range(1,len(fonkList)+1):
+                a = input("1. Sayıyı Giriniz:")
+                b = input("2. Sayıyı Giriniz:")
+                if (a and b) and (a.isdigit() and a.isdigit()):
+                    sonuc = fonkList[islem-1](int(a),int(b))
+                    print(f"{sonuc[0]} {sonuc[3]} {sonuc[1]} = {sonuc[2]}")
+                else:
+                    print("Veri Girişi Hatası")
+            elif islem != 7:
+                print("İşlem Seçimi Hatası")
+        else:
+            print("İşlem Girişi Hatası")
+    else:
+        print("İyi Günler Dileriz")
+
+hesapMak()
--- ilk.py
+++ ilk.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 liste = ["HakanOzden","HakanOzel","GamzeCelik","FurkanKeles","MerveSarpBoyar","OzgeCillik","SimgeAydin","IpekDolu"]
-fileName = "03_01_fonksiyonlar.ipynb"
+fileName = "03_02_fonksiyonlar.ipynb"
 for item in liste:
     if not os.path.exists(f"Egzersizler/{item}"):
         os.mkdir(f"Egzersizler/{item}")
